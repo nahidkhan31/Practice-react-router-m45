48984f2983deda7f33a07490a8fde1c78b05f356
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-04-17 14:41:21 +0600

    added first commit

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from "./components/Root/Root.jsx";
+import Home from "./components/Home/Home.jsx";
+import Mobiles from "./components/Mobiles/Mobiles.jsx";
+import Laptops from "./components/Laptops/Laptops.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: "mobiles", Component: Mobiles },
+      { path: "laptops", Component: Laptops },
+    ],
+  },
+  {
+    path: "about",
+    element: <div>About me here</div>,
+  },
+  {
+    path: "blogs",
+    element: <div>All my blogs are here</div>,
+  },
+  {
+    path: "app",
+    Component: App,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
